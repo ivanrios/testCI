25921988105cfe03d58cb5944f477e4fcc4036cd
Ivan Rios <ivanrios.a@gmail.com>
2017-07-25 10:47:26 -0500

    Se agrega soporte para circleCI

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ var app = express();
 var exports = module.exports = {};
 
 app.get('/', function(req, res){
-  res.send('Servidor Activo');
+  res.send('Servidor ActivoX');
 });
 
 var server = app.listen(3000, function(){
