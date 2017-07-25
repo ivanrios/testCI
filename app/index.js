3a40763ab321f8892cb37424421311c3d93e9697
Ivan Rios <ivanrios.a@gmail.com>
2017-07-25 11:45:25 -0500

    se corrige

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ var app = express();
 var exports = module.exports = {};
 
 app.get('/', function(req, res){
-  res.send('Servidor Activoxx');
+  res.send('Servidor Activo');
 });
 
 var server = app.listen(3000, function(){
